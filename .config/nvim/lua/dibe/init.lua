faf587a1e8fd37c8933ca397b2b9ff3484840f9e
W.D <djvoleur@gmail.com>
2022-12-16 14:23:04 -0700

    Update init.lua

--- .config/nvim/lua/dibe/init.lua
+++ .config/nvim/lua/dibe/init.lua
@@ -1,3 +1,14 @@
 require("dibe.remap")
 require("dibe.packer")
 require("dibe.set")
+
+local augroup = vim.api.nvim_create_augroup
+local ThePrimeagenGroup = augroup('ThePrimeagen', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd({"BufWritePre"}, {
+    group = ThePrimeagenGroup,
+    pattern = "*",
+    command = "%s/\\s\\+$//e",
+})
