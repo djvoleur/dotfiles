d758d8fdddb2df88e10d70c15c2078ee9807aa97
W.D <djvoleur@gmail.com>
2022-12-17 12:57:05 -0700

    Switch to nvim-lua's kickstart
    https://github.com/nvim-lua/kickstart.nvim

--- setup.sh
+++ setup.sh
@@ -2,10 +2,4 @@
 
 dir=$('pwd')
 
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-
 ln -s ${dir}/.config/nvim/init.lua $HOME/.config/nvim/init.lua
-ln -s ${dir}/.config/nvim/after $HOME/.config/nvim/after
-ln -s ${dir}/.config/nvim/lua $HOME/.config/nvim/lua
-ln -s ${dir}/.config/nvim/plugin $HOME/.config/nvim/plugin
