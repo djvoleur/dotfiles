16c63a769147dcab421595a8ef03a6645832a4b9
W.D <djvoleur@gmail.com>
2022-12-19 08:31:08 -0700

    Switch <leader>pv location

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -141,8 +141,8 @@ vim.g.maplocalleader = ' '
 
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
