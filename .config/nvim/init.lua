e3d93aa3cde7959d6e4c3b8169577ee0f96815eb
WD <djvoleur@gmail.com>
2023-05-29 12:11:03 -0700

    Remove bashls, pyright, and terraform-ls

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -368,7 +368,7 @@ require('mason').setup()
 
 -- Enable the following language servers
 -- Feel free to add/remove any LSPs that you want here. They will automatically be installed
-local servers = { 'bashls', 'rust_analyzer', 'pyright', 'lua_ls', 'gopls', 'terraformls' }
+local servers = { 'rust_analyzer', 'lua_ls', 'gopls' }
 
 -- Ensure the servers above are installed
 require('mason-lspconfig').setup {
