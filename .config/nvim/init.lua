ffb2ca75a2946ffb14bb78b6146de65af40beba8
WD <djvoleur@gmail.com>
2023-06-01 21:25:38 -0700

    Update LSPs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -368,7 +368,7 @@ require('mason').setup()
 
 -- Enable the following language servers
 -- Feel free to add/remove any LSPs that you want here. They will automatically be installed
-local servers = { 'rust_analyzer', 'lua_ls', 'gopls' }
+local servers = { 'rust_analyzer', 'lua_ls', 'gopls', 'bashls', 'pylsp' }
 
 -- Ensure the servers above are installed
 require('mason-lspconfig').setup {
