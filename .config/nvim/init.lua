938c404945bbcb97be8b929763d783558be5b5ee
WD <djvoleur@gmail.com>
2023-06-01 21:19:18 -0700

    Add some things 'n stuffs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -237,7 +237,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'bash', 'go', 'hcl', 'lua', 'python', 'rust', 'help' },
+  ensure_installed = { 'bash', 'go', 'hcl', 'lua', 'python', 'rust' },
 
   highlight = { enable = true },
   indent = { enable = true },
--- setup.sh
+++ setup.sh
@@ -6,4 +6,9 @@ if [ ! -d "$HOME/.config/nvim" ]; then
   mkdir $HOME/.config/nvim
 fi
 
-ln -s ${dir}/.config/nvim/init.lua $HOME/.config/nvim/init.lua
+if [ -L $HOME/.config/nvim/init.lua ] && [ -f $HOME/.config/nvim/init.lua ]; then
+  rm -f $HOME/.config/nvim/init.lua
+  ln -s ${dir}/.config/nvim/init.lua $HOME/.config/nvim/init.lua
+else
+  ln -s ${dir}/.config/nvim/init.lua $HOME/.config/nvim/init.lua
+fi
