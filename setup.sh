78e82fdce75c516895bc13476ced6b5ddc57c7c9
WD <djvoleur@gmail.com>
2023-06-02 21:23:21 -0700

    Updates

--- setup.sh
+++ setup.sh
@@ -4,18 +4,20 @@ OS=$(grep "ID" /etc/os-release | head -n 1 | cut -d "=" -f 2)
 export GIT_EDITOR=nvim
 
 if [ "$OS" == "fedora" ]; then
-   sudo dnf install -y golang rust-analyzer npm gcc-c++ git
+  # Setup for neovim
+  sudo dnf install -y golang rust-analyzer npm gcc-c++ git fd-find ripgrep cargo python3-pip zsh
+  /usr/bin/python3 -m pip install pynvim
+fi
+# Setup zsh-autosuggestions
+if [ ! -d $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
 
-if [ ! -d "$HOME/.config/nvim" ]; then
-  mkdir $HOME/.config/nvim
-fi
+mkdir -p $HOME/.config/nvim
 
-if [ -L $HOME/.config/nvim/init.lua ] && [ -f $HOME/.config/nvim/init.lua ]; then
-  rm -f $HOME/.config/nvim/init.lua
-  ln -s ${dir}/.config/nvim/init.lua $HOME/.config/nvim/init.lua
-else
-  ln -s ${dir}/.config/nvim/init.lua $HOME/.config/nvim/init.lua
-fi
+rm -f $HOME/.config/nvim/init.lua
+rm -f $HOME/.zshrc
+ln -s ${dir}/config/nvim/init.lua $HOME/.config/nvim/init.lua
+ln -s ${dir}/.zshrc $HOME/.zshrc
 
 git config --global core.editor "nvim"
